3a3d5cd55589876226604d8a42bb589a4ff79bde
unwindflz <gouxinshun217@163.com>
2025-01-17 00:35:40 +0800

    test: add additional test cases for date_utils

---
+++ tests/unit/test_date_utils_v4.py
@@ -0,0 +1,12 @@
+import unittest
+from src.utils.date_utils import some_function
+
+class TestDateUtils(unittest.TestCase):
+    def test_some_function(self):
+        # Test case for some_function
+        result = some_function(args)
+        expected = expected_value
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
